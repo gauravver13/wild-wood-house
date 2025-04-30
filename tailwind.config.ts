77f5fd6015277c958c7ec3b466dd157f69f417ba
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-05-01 04:33:00 +0530

    figured out in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -82,7 +82,13 @@ module.exports = {
 		animation: {
 			scroll: 'scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite',
 		},
-		
+		keyframes: {
+			scroll: {
+			  to: {
+				transform: 'translate(calc(-50% - 0.5rem))',
+			  },
+			},
+		  },
 		borderRadius: {
 		  lg: `var(--radius)`,
 		  md: `calc(var(--radius) - 2px)`,
